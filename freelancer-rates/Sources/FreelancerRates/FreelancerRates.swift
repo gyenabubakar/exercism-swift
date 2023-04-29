8033667ad3de63481d607b3de4ef264d75c16915
Gyen Abubakar <indehyde@gmail.com>
2023-04-29 12:03:06 +0000

    solved exercism: freelancer-rates

--- freelancer-rates/Sources/FreelancerRates/FreelancerRates.swift
+++ freelancer-rates/Sources/FreelancerRates/FreelancerRates.swift
@@ -1,11 +1,23 @@
+import Foundation
+
+let hoursPerDay = 8;
+let billableDaysInMonth = 22;
+
 func dailyRateFrom(hourlyRate: Int) -> Double {
-  fatalError("Please implement the dailyRateFrom(hourlyRate:) function")
+  return Double(hoursPerDay) * Double(hourlyRate)
 }
 
 func monthlyRateFrom(hourlyRate: Int, withDiscount discount: Double) -> Double {
-  fatalError("Please implement the monthlyRateFrom(hourlyRate:withDiscount:) function")
+  let daily = dailyRateFrom(hourlyRate: hourlyRate)
+  let month = daily * Double(billableDaysInMonth)
+  let discounted = (discount / 100) * month
+  return (month - discounted).rounded()
 }
 
 func workdaysIn(budget: Double, hourlyRate: Int, withDiscount discount: Double) -> Double {
-  fatalError("Please implement the workdaysIn(budget:hourlyRate:withDiscount:) function")
+  let daily = dailyRateFrom(hourlyRate: hourlyRate)
+  let discounted = (discount / 100) * daily
+  let dailyWithDiscount = daily - discounted
+  let days = budget / dailyWithDiscount
+  return floor(days)
 }
