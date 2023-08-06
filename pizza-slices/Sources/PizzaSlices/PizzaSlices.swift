3db6f35b7ad933f20531027a0a461547730a482e
Gyen Abubakar <indehyde@gmail.com>
2023-08-06 11:03:12 +0000

    solved exercise: pizza-slices

--- pizza-slices/Sources/PizzaSlices/PizzaSlices.swift
+++ pizza-slices/Sources/PizzaSlices/PizzaSlices.swift
@@ -1,10 +1,34 @@
+import Foundation
+
 func sliceSize(diameter: Double?, slices: Int?) -> Double? {
-  fatalError("Please implement the sliceSize(diameter:slices:) function")
+  guard case let (diameter?, slices?) = (diameter, slices),
+        diameter >= 0 && slices >= 1 else {
+    return nil
+  }
+  let radius = diameter / 2
+  return (Double.pi * pow(radius, 2)) / Double(slices)
 }
 
 func biggestSlice(
   diameterA: String, slicesA: String,
   diameterB: String, slicesB: String
 ) -> String {
-  fatalError("Please implement the biggestSlice(diameterA:slicesA:diameterB:slicesB:) function")
+  let pizzaA = (diameter: Double(diameterA), slices: Int(slicesA))
+  let pizzaB = (diameter: Double(diameterB), slices: Int(slicesB))
+
+  let areaA = sliceSize(diameter: pizzaA.diameter, slices: pizzaA.slices)
+  let areaB = sliceSize(diameter: pizzaB.diameter, slices: pizzaB.slices)
+
+  switch (areaA, areaB) {
+    case (let a, nil) where a != nil:
+      return "Slice A is bigger"
+    case (nil, let b) where b != nil:
+      return "Slice B is bigger"
+    case let (a?, b?) where a > b:
+      return "Slice A is bigger"
+    case let (a?, b?) where b > a:
+      return "Slice B is bigger"
+    default:
+      return "Neither slice is bigger"
+  }
 }
